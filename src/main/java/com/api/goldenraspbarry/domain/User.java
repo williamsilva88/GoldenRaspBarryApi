985b102520073bf2ab57e2f5367f3adde91a409d
williamsilva88 <william.silva88@outlook.com>
2022-11-26 21:43:09 -0300

    Projeto inicial

---
+++ src/main/java/com/api/goldenraspbarry/domain/User.java
@@ -0,0 +1,22 @@
+package com.api.goldenraspbarry.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
